6eee96bf65f4c9cc630785860026599f3cb302ee
Shivaths Karne <karneshivaths@gmail.com>
2026-01-07 00:20:55 -0500

    initial commit

---
+++ src/main/java/net/shivaths/VienMinerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.shivaths;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VienMinerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
